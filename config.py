a91027d8385ca086be9b4b46a6cae6112ec37ce3
hezron-ngoma-hezzykialo <hezzykialo.official.ke@gmail.com>
2022-05-17 21:44:37 +0300

    UI design for lyrics

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ class ProdConfig(Config):
 
 
 class DevConfig(Config):
-  SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://username:password@localhost:5432/truebarz'
+  SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://hezron:09876@localhost:5432/truebarz'
   DEBUG = True
   ASSETS_DEBUG = True
   
