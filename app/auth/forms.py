13e02fcdf16c2dafb6352452d8f8ee2ccfeabb26
Paivy <paivyeshirera@gmail.com>
2022-05-17 22:38:20 +0300

    added registration page

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -10,3 +10,17 @@ class LoginForm(FlaskForm):
     password = StringField('Your password',validators=[InputRequired()])
     rememberMe = BooleanField('Remember')
     submit = StringField('Sign In ')
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Your Email Address' ,validators=[Required(), Email()])
+    username = StringField('Enter your useraname', validators= [Required()])
+    password = PasswordField('Password',validators=[Required(), EqualTo('password_confirm',message ='Password must match')])
+    password_confirm = PasswordField('Confirm Password',validators=[Required()])
+    submit= SubmitField('Sign Up')
+
+    def validate_email(self,data_field):
+        if User.query.filter_by(email = data_file.data).first():
+            raise ValidationError('There is an account with that email')
+    def validate_username(self,data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError('That username is taken')
--- app/auth/views.py
+++ app/auth/views.py
@@ -5,6 +5,18 @@ from .. import db
 from . import auth
 from ..models import User
 
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form =form)
+
+
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
