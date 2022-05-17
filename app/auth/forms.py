0bcaaef616a54364780ff884a1babd175ec59062
Ann Wanjeri <wannjeri2@gmail.com>
2022-05-17 21:49:59 +0300

    created class login form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,3 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField,ValidationError,BooleanField
 from wtforms.validators import InputRequired,Email,EqualTo,Length
+from wtforms import ValidationError
+
+
+# login form
+class LoginForm(FlaskForm):
+    email = StringField('Your email address',validators=[InputRequired(),Email()])
+    password = StringField('Your password',validators=[InputRequired()])
+    rememberMe = BooleanField('Remember')
+    submit = StringField('Sign In ')
