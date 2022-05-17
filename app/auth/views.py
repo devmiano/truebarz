db008d0e49857b513559b200e7dc075c35d27f10
Paivy <paivyeshirera@gmail.com>
2022-05-17 14:59:25 +0300

    created the form instance

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,21 @@
 from flask import render_template,redirect,url_for,flash,request
 from flask_login import login_user,logout_user, login_required
+from .forms import RegistrationForm
+from ..models import User
 
 from .. import db
 from . import auth
+
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form =form)
+
+
+
