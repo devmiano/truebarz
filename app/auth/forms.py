1a41ecec8a23c2d263db97d691c2a0199962716c
Paivy <paivyeshirera@gmail.com>
2022-05-17 12:55:25 +0300

    created the registration form class

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,3 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField,ValidationError,BooleanField
 from wtforms.validators import InputRequired,Email,EqualTo,Length
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Your Email Address' ,validators=[Required(), Email()])
+    username = StringField('Enter your useraname', validators= [Required()])
+    password = PasswordField('Password',validators=[Required(), EqualTo('password_confirm',message ='Password must match')])
+    password_confirm = PasswordField('Confirm Password',validators=[Required()])
+    submit= SubmitField('Sign Up')
